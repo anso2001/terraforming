df1a5550ee68638adabe660e473a2206c4e1f8ee
herr-hxp <haxxpet@gmail.com>
2021-08-11 09:08:31 +0200

    changed file convention to bp

--- main.tf
+++ main.tf
@@ -1,3 +1,55 @@
+## INIT
+# Define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  #user_name   = "" # use $OS_USERNAME
+  #tenant_name = "" # use $OS_PROJECT_NAME
+  #tenant_id    = "" # use $OS_PROJECT_ID
+  #password    = "" # use $OS_PASSWORD
+  #auth_url    = "" # use $OS_AUTH_URL
+  #region      = "" # use $OS_REGION_NAME
+}
+
+## INSTANCES
+# Create an instance
+resource "openstack_compute_instance_v2" "instance_1" {
+  name              = "testing"
+  image_id          = "aa97b237-095a-4edf-8ddd-4a18d3c6a8d4" # ubuntu-20.04-server-latest
+  # ^ can also use image_name
+  flavor_id         = "7a6a998f-ac7f-4fb8-a534-2175b254f75e" # v1-mini-1
+  # ^ can also use flavor_name 
+  key_pair          = "micke"
+  #security_groups   = ["default","${openstack_compute_secgroup_v2.secgroup_1.name}"]
+  # region           = ""
+  # count            = 
+
+
+  network {
+      port = "${openstack_networking_port_v2.port_1.id}"
+  }
+
+  # DON'T NEED THIS FOR NOW
+  # create a volume and attach it
+  # block_device {
+  #     source_type           = "blank"
+  #     destination_type      = "volume"
+  #     volume_size           = 10
+  #     # boot_index            = 1
+  #     delete_on_termination = true
+  # }
+}
+
+## NETWORK
 # Create network
 resource "openstack_networking_network_v2" "network_1" {
   name           = "tf_network"
