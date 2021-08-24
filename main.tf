75b7aa9188fa65abbe5e1229a131a868b20cf37f
herr-hxp <haxxpet@gmail.com>
2021-08-24 13:38:22 +0200

    added versions.tf and moved that info there

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-## INIT
-# Define required providers
-terraform {
-required_version = ">= 0.14.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.35.0"
-    }
-  }
-}
-
 # Configure the OpenStack Provider
 provider "openstack" {
   #user_name   = "" # use $OS_USERNAME
