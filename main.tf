283c92745dbdec15e0382ab21e8a4ab57b4b5327
herr-hxp <haxxpet@gmail.com>
2021-09-30 20:36:30 +0200

    added security groups for ssh and http and htts

--- main.tf
+++ main.tf
@@ -1,98 +1,103 @@
+## PROVIDER
 # Configure the OpenStack Provider
 provider "openstack" {
-  #user_name   = "" # use $OS_USERNAME
-  #tenant_name = "" # use $OS_PROJECT_NAME
-  #tenant_id    = "" # use $OS_PROJECT_ID
-  #password    = "" # use $OS_PASSWORD
-  #auth_url    = "" # use $OS_AUTH_URL
-  #region      = "" # use $OS_REGION_NAME
-}
-
-## INSTANCE(S)
-# Create an instance
-resource "openstack_compute_instance_v2" "instance_1" {
-  name              = var.instance_name
-  image_id          = var.image_id # name: ubuntu-20.04-server-latest
-  # ^ can also use image_name
-  flavor_id         = var.flavor_id # v1-mini-1
-  # ^ can also use flavor_name 
-  key_pair          = var.key_name
-  #security_groups   = ["default","${openstack_compute_secgroup_v2.secgroup_1.name}"]
-  # region           = ""
-  # count            = 
-
-
-  network {
-      port = "${openstack_networking_port_v2.port_1.id}"
-  }
-
-  # DON'T NEED THIS FOR NOW
-  # create a volume and attach it
-  # block_device {
-  #     source_type           = "blank"
-  #     destination_type      = "volume"
-  #     volume_size           = 10
-  #     # boot_index            = 1
-  #     delete_on_termination = true
-  # }
+  #user_name          = "" # use $OS_USERNAME
+  #tenant_name        = "" # use $OS_PROJECT_NAME
+  #tenant_id          = "" # use $OS_PROJECT_ID
+  #password           = "" # use $OS_PASSWORD
+  #auth_url           = "" # use $OS_AUTH_URL
+  #region             = "" # use $OS_REGION_NAME
 }
 
 ## NETWORK
 # Create network
 resource "openstack_networking_network_v2" "network_1" {
-  name           = var.network_name
-  admin_state_up = "true"
+  name                = var.network_name
+  admin_state_up      = "true"
 }
 
 # Create subnet
 resource "openstack_networking_subnet_v2" "subnet_1" {
-  name            = var.subnet_name
-  network_id      = "${openstack_networking_network_v2.network_1.id}"
-  cidr            = var.subnet_cidr
-  ip_version      = 4
-  dns_nameservers = var.dns_ip
+  name                = var.subnet_name
+  network_id          = "${openstack_networking_network_v2.network_1.id}"
+  cidr                = var.subnet_cidr
+  ip_version          = 4
+  dns_nameservers     = var.dns_ip
 }
 
-# Create Security Group
+# Create Security Groups
 resource "openstack_compute_secgroup_v2" "secgroup_1" {
-  name        = "TF SG SSH"
-  description = "Terraform SG for external SSH"
+  name                = "TerraformSG"
+  description         = "Terraform SG for Port 80 and 443"
 
   rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
+    from_port         = 80
+    to_port           = 80
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port         = 443
+    to_port           = 443
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "secgroup_2" {
+  name                = "TerraformSSH"
+  description         = "Terraform SH for Port 22"
+
+  rule {
+    from_port         = 22
+    to_port           = 22
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0" # only for demo purposes, tighten up in live scenario
   }
 }
 
 # Create a port
 resource "openstack_networking_port_v2" "port_1" {
-  name               = "port_1"
-  network_id         = "${openstack_networking_network_v2.network_1.id}"
-  admin_state_up     = "true"
-  security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+  name                = "port_1"
+  network_id          = "${openstack_networking_network_v2.network_1.id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
   fixed_ip {
-    subnet_id  = "${openstack_networking_subnet_v2.subnet_1.id}"
-    ip_address = var.port_ip
+    subnet_id         = "${openstack_networking_subnet_v2.subnet_1.id}"
+    ip_address        = var.port_ip
   }
 }
 
 # Connect the subnet to the router
-#+ later: create a new router from scratch
 resource "openstack_networking_router_interface_v2" "router_interface_1" {
-  router_id = var.router_id # replace with new router when created "${openstack_networking_router_v2.router_1.id}"
-  subnet_id = "${openstack_networking_subnet_v2.subnet_1.id}"
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnet_1.id}"
 }
 
 # Allocate Floating IP
 resource "openstack_networking_floatingip_v2" "floatip_1" {
-  pool = var.fip_pool
+  pool                = var.fip_pool
+}
+
+## INSTANCE
+# Create an instance
+resource "openstack_compute_instance_v2" "instance_1" {
+  name                = var.instance_name
+  image_name          = var.image_name
+  flavor_name         = var.flavor_name
+  key_pair            = var.key_name
+  security_groups     = ["default","${openstack_compute_secgroup_v2.secgroup_1.name}","${openstack_compute_secgroup_v2.secgroup_2.name}"]
+  user_data           = var.cloudconfig_web
+
+  network {
+    port              = "${openstack_networking_port_v2.port_1.id}"
+  }
 }
 
 # Associate Floating IP
 resource "openstack_networking_floatingip_associate_v2" "fip_1" {
-  floating_ip = "${openstack_networking_floatingip_v2.floatip_1.address}"
-  port_id     = "${openstack_networking_port_v2.port_1.id}"
+  floating_ip         = "${openstack_networking_floatingip_v2.floatip_1.address}"
+  port_id             = "${openstack_networking_port_v2.port_1.id}"
 }
